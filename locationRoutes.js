bc46108d591688891d1b6258ee479bb4d1c26229
Akshitha Gunda <akshithasgunda@gmail.com>
2026-06-04 17:38:33 +0530

    Remove duplicate root files

--- locationRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const controller = require('../controllers/locationController');
-
-router.get('/autocomplete', controller.searchVillages);
-
-module.exports = router;
-
-
